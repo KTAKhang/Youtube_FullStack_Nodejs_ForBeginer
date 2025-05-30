69c0a5bab037ebcd75ff01011251f7f4ce2a5c79
KTAKhang <khangkien.060603@gmail.com>
2025-05-30 08:17:56 +0700

    add auto render swagger v2 10 minutes

--- src/jobs/callApiJob.js
+++ src/jobs/callApiJob.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const axios = require('axios');
 
 // Gọi API cứ mỗi 10 phút
-cron.schedule(' */30 * * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
     try {
         const response = await axios.get('https://youtube-fullstack-nodejs-forbeginer.onrender.com/api/product?page=1&limit=10');
         console.log('[Cron] API gọi thành công lúc:', new Date(), response.data);
